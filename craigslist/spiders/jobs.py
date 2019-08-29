240d0568291ee322f5d21d470dfe7b59b70e7642
C J <cjschamith+pp@gmail.com>
2019-08-29 20:23:15 +0530

    scrape the date, title, link from the first page

--- craigslist/spiders/jobs.py
+++ craigslist/spiders/jobs.py
@@ -5,7 +5,20 @@ import scrapy
 class JobsSpider(scrapy.Spider):
     name = 'jobs'
     allowed_domains = ['losangeles.craigslist.org']
-    start_urls = ['http://losangeles.craigslist.org/']
+    start_urls = ['https://losangeles.craigslist.org/search/egr']
 
     def parse(self, response):
-        pass
+        listings = response.xpath('//li[@class="result-row"]')
+
+        for listing in listings:
+            date = listing.xpath('.//time/@datetime').get()
+            title = listing.xpath(
+                './/a[@class="result-title hdrlnk"]/text()').get()
+            link = listing.xpath(
+                './/a[@class="result-title hdrlnk"]/@href').get()
+
+            yield {
+                'date': date,
+                'title': title,
+                'link': link
+            }
